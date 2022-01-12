98dc485738883f428e3e6330cad293ef0780325e
AngeloLista <abstractplayer@gmail.com>
2022-01-12 18:19:14 +0100

    added for cycle method for email exercise

--- js/script-email.js
+++ js/script-email.js
@@ -8,9 +8,25 @@ let emailList = ['marcorossi@gmail.com', 'vincenzoferrari@gmail.com', 'francesco
 
 const userEmail = prompt('Inserisci la tua Email');
 
+/* METODO CORTO
 const risultato = emailList.includes(userEmail)
 
 if (risultato) {
+    console.log("La tua Email è presente nell'elenco")
+} else {
+    console.log("La tua Email non è presente nell'elenco")
+}*/
+
+
+let isAllowed = false;
+
+for (let i = 0; i < emailList.length; i++) {
+    if(userEmail === emailList[i]) {
+        isAllowed = true;
+    }
+}
+
+if (isAllowed) {
     console.log("La tua Email è presente nell'elenco")
 } else {
     console.log("La tua Email non è presente nell'elenco")
